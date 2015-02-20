40a11cf60d4d758a0172f763d7e527bbe97d2345
Sandro Dolidze <sdolidze3@gmail.com>
2015-02-20 23:47:41 +0400

    cleaned Folding module

--- java/src/main/java/fp/Folding.java
+++ java/src/main/java/fp/Folding.java
@@ -3,10 +3,6 @@ package fp;
 import java.util.function.BiFunction;
 
 public class Folding {
-    private static int sumRight(List<Integer> xs) {
-        return xs == null ? 0 : xs.head + sumRight(xs.tail);
-    }
-
     public static<A,B> B foldRightRec(BiFunction<A, B, B> f, B v, List<A> xs) {
         return xs == null ? v : f.apply(xs.head, foldRightRec(f, v, xs.tail));
     }
@@ -14,14 +10,6 @@ public class Folding {
     public static<A,B> B foldRightIter(BiFunction<A, B, B> f, B v, List<A> xs) {
         // todo: there must be a way, other than reversal
         return foldLeftIter(Prelude.flip(f), v, Prelude.reverse(xs));
-    }
-
-    private static int sumFoldRight(List<Integer> xs) {
-        return foldRightRec((a, b) -> a + b, 0, xs);
-    }
-
-    private static int sumLeft(int acc, List<Integer> xs) {
-        return xs == null ? acc : sumLeft(acc+xs.head, xs.tail);
     }
 
     public static<A,B> B foldLeftRec(BiFunction<B, A, B> f, B v, List<A> xs) {
@@ -35,9 +23,5 @@ public class Folding {
             xs = xs.tail;
         }
         return acc;
-    }
-
-    private static int sumFoldLeft(List<Integer> xs) {
-        return foldLeftRec((a, b) -> a + b, 0, xs);
     }
 }
--- java/src/main/java/fp/Integers.java
+++ java/src/main/java/fp/Integers.java
@@ -4,12 +4,32 @@ package fp;
  * Created by sandro on 2/20/15.
  */
 public class Integers {
+    /*
+     * left association => sum([1,2,3]) = (((0+1)+2)+3)
+     */
+    private static int sumLeft(int acc, List<Integer> xs) {
+        return xs == null ? acc : sumLeft(acc + xs.head, xs.tail);
+    }
+
     public static Integer sum(List<Integer> xs) {
         return Prelude.foldLeft((x, y) -> x + y, 0, xs);
+    }
+
+    /*
+     * right association => sum([1,2,3]) = (1+(2+(3+0)))
+     * addition of integers is associative,
+     * therefor left fold and right yield the same result
+     * (division does not)
+     */
+    private static int sumRight(List<Integer> xs) {
+        return xs == null ? 0 : xs.head + sumRight(xs.tail);
+    }
+
+    private static int sumFoldRight(List<Integer> xs) {
+        return Prelude.foldRight((x, y) -> x + y, 0, xs);
     }
 
     public static Integer product(List<Integer> xs) {
         return Prelude.foldLeft((x, y) -> x * y, 1, xs);
     }
-
 }
